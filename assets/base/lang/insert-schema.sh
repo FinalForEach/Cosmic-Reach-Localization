7cb2e6cb11b55686bd109be341fcc392d357940c
FinalForEach <finalforeach@gmail.com>
2024-09-14 22:14:40 +0100

    Tip schema

--- assets/base/lang/insert-schema.sh
+++ assets/base/lang/insert-schema.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # The line to be inserted
-insert_line='  "$schema": "https://https://raw.githubusercontent.com/FinalForEach/Cosmic-Reach-Localization/master/assets/base/lang/game.schema.json",'
+insert_line='  "$schema": "https://https://raw.githubusercontent.com/FinalForEach/Cosmic-Reach-Localization/master/assets/base/lang/tip.schema.json",'
 
 # Loop through each folder and find game.json files
-find . -type f -name "game.json" | while read -r file; do
+find . -type f -name "tip.json" | while read -r file; do
   # Insert the line at line 2 using sed
   sed -i '2i\'"$insert_line" "$file"
 done
